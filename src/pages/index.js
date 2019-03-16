905a34eec8466807c84d9080d8e09f16807de1a5
Lola <lolahef@gmail.com>
2019-03-16 18:09:56 -0400

    Updated link color

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ class BlogIndex extends React.Component {
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                <Link style={{ boxShadow: `none`, color: `#4564a8` }} to={node.fields.slug}>
                   {title}
                 </Link>
               </h3>
